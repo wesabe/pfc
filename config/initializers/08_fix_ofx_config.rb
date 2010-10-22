c8a91485e868f067a58c56926d122b0a03e7ced3
Brian Donovan <me@brian-donovan.com>
2010-10-23 01:35:53 +0800

    Prefer a 'fixofx' binary at Rails root.

--- config/initializers/08_fix_ofx_config.rb
+++ config/initializers/08_fix_ofx_config.rb
@@ -1,12 +1,13 @@
 # if you are running this locally, comment out the following line
 FIXOFX_TIMEOUT = 120
 
-# prefer the version installed in /opt, if it exists
-if File.exists?("/opt/fixofx/timeout")
+if (fixofx = Rails.root.join('fixofx')).exist?
+  OFX_CONVERTER = fixofx
+elsif File.exist?("/opt/fixofx/timeout")
   OFX_CONVERTER = "/opt/fixofx/timeout #{FIXOFX_TIMEOUT} /opt/fixofx/fixofx"
-elsif File.exists?("/var/wesabe/util/fixofx/timeout")
+elsif File.exist?("/var/wesabe/util/fixofx/timeout")
   OFX_CONVERTER = "/var/wesabe/util/fixofx/timeout #{FIXOFX_TIMEOUT} /var/wesabe/util/fixofx/fixofx"
-elsif File.exists?(Rails.root.join('../fixofx/fixofx.py'))
+elsif Rails.root.join('../fixofx/fixofx.py').exist?
   path = Rails.root.join('../fixofx/fixofx.py')
   OFX_CONVERTER = "python #{path}"
 end
--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,1 +1,2 @@
 run "rake assets:roll"
+run "test -e #{shared_path}/fixofx && ln -nfs #{shared_path}/fixofx #{release_path}/fixofx"
