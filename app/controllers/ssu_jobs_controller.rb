7e32c76e8fb19a5d700549053db2489b52f46873
Brian Donovan <me@brian-donovan.com>
2010-11-01 09:35:28 -0700

    Use the newer presenter stuff.

--- app/controllers/ssu_jobs_controller.rb
+++ app/controllers/ssu_jobs_controller.rb
@@ -1,35 +1,20 @@
 class SsuJobsController < ApplicationController
   before_filter :check_authentication, :except => [:update]
 
-  def update
-    @job.update_status(params)
-    render :json => @job.presenter.to_internal_json
-  end
-
   def create
-    if job = account_cred.enqueue_sync
-      @job = job.presenter
+    if @job = account_cred.enqueue_sync
       respond_to do |format|
-        format.json { render :json => @job.to_json, :status => :created, :location => credential_job_url(account_cred, job) }
+        format.json { render :json => present(@job), :status => :created, :location => credential_job_url(account_cred, @job) }
       end
     else
       render :text => 'unable to start job', :status => :bad_gateway
     end
   end
 
-  def index
-    @jobs = @account_cred.all_ssu_jobs.map {|job| job.presenter}
-    respond_to do |format|
-      format.xml { render :action => 'index' } # index.xml.builder
-      format.js  { render :action => 'index' } # index.js.erb
-    end
-  end
-
   def show
-    if job = account_cred.jobs.find_by_job_guid(params[:id])
-      @job = job.presenter
+    if @job = account_cred.jobs.find_by_job_guid(params[:id])
       respond_to do |format|
-        format.json { render :json => @job.to_json }
+        format.json { render :json => present(@job) }
       end
     else
       render :nothing => true, :status => :not_found
