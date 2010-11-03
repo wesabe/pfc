53872040d60831de08fbbe95352a54f83c4e557d
Brian Donovan <me@brian-donovan.com>
2010-11-03 09:17:49 -0700

    Symlink ssu into place on deploy.

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,2 +1,3 @@
 run "rake assets:roll"
 run "test -e #{shared_path}/fixofx && ln -nfs #{shared_path}/fixofx #{release_path}/fixofx"
+run "test -e #{shared_path}/ssu    && ln -nfs #{shared_path}/ssu    #{release_path}/vendor/ssu"
