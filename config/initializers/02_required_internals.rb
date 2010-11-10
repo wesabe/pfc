81439317821365ce54bec0ce3d9d965547a1f83e
Brian Donovan <me@brian-donovan.com>
2010-11-10 07:27:58 -0800

    Missed a spot.

--- config/initializers/02_required_internals.rb
+++ config/initializers/02_required_internals.rb
@@ -3,7 +3,6 @@ require "acts_as_taggable"
 require "api/constants"
 require "constants"
 require "crypto"
-require "ssu_crypto"
 require "makeofx2"
 require 'txaction_filter'
 require "ofx2importer"
