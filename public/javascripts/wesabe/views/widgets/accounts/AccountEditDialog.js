wesabe/pfc

commit 91573cc32cff9ae7ab6435a32d0207a4d63402da

Use BRCM's account editing features via JS.

public/javascripts/wesabe/views/widgets/accounts/AccountEditDialog.js
@@ -164,43 +164,92 @@ wesabe.$class('wesabe.views.widgets.accounts.AccountEditDialog', wesabe.views.wi
     },
 
     onSave: function() {
-      var me = this;
+      var me = this,
+          account = me._account,
+          dirty = false,
+          accountURI = "/data"+me._account.getURI(),
+          shouldTrackBalance = me._trackBalanceBox.attr('checked');
 
-      var saveData = {
-        name: me._name.val(),
-        currency: me._currency.val(),
-        enable_balance: me._trackBalanceBox.attr('checked'),
-        current_balance: me._currentBalance.val(),
-        status: me._archivedBox.attr('checked') ? 'archived' : 'active'
-      };
+      function commitAttributes() {
+        var data = {
+          name: me._name.val(),
+          currency: me._currency.val()
+        };
 
-      $.ajax({
-        type: "PUT",
-        data: saveData,
-        url: me._account.getURI(),
-        dataType: "json",
-        beforeSend: function() {
-          me._spinner.show();
-          me.setButtonsDisabled(true);
-        },
-        success: function(data, textStatus) {
-          if (data.status == 'active') {
-            me._account.update(data);
-          } else {
-            // Hide archived accounts immediately
-            me._account.remove();
-          }
-          me._accountWidget.refresh();
-          me.onEndEdit();
-        },
-        error: function(XMLHttpRequest, textStatus, errorThrown) {
-          me._saveError.slideDown();
-        },
-        complete: function() {
-          me._spinner.hide();
-          me.setButtonsDisabled(false);
+        if (data.name === account.getName() && data.currency === account.getCurrency()) {
+          // nothing changed
+          enableOrDisableBalance();
+        } else {
+          dirty = true;
+          $.ajax({
+            type: "PUT",
+            url: accountURI,
+            data: data,
+            beforeSend: busy,
+            success: enableOrDisableBalance,
+            error: error
+          });
         }
-      });
+      }
+
+      function enableOrDisableBalance() {
+        if (!account.isCash() || !(shouldTrackBalance ^ account.hasBalance())) {
+          // can't do anything about it or nothing has changed
+          commitBalance();
+        } else {
+          dirty = true;
+          $.ajax({
+            type: "PUT",
+            url: accountURI+"/"+(shouldTrackBalance ? "enable" : "disable")+"-balance",
+            beforeSend: busy,
+            success: commitBalance,
+            error: error
+          })
+        }
+      }
+
+      function commitBalance() {
+        var newBalance = me._currentBalance.val();
+
+        if (!shouldTrackBalance || !hasValue(newBalance) || (newBalance === "") || (newBalance === account.getTotal())) {
+          // not tracking balance or nothing changed
+          notBusy();
+          done();
+        } else {
+          dirty = true;
+          $.ajax({
+            type: "POST",
+            url: accountURI+"/balances",
+            data: {balance: newBalance && newBalance.replace(/[^\d\.,]+/g, '')},
+            beforeSend: busy,
+            success: done,
+            error: error,
+            complete: notBusy
+          });
+        }
+      }
+
+      function busy() {
+        me._spinner.show();
+        me.setButtonsDisabled(true);
+      }
+
+      function notBusy() {
+        me._spinner.hide();
+        me.setButtonsDisabled(false);
+      }
+
+      function error() {
+        me._saveError.slideDown();
+      }
+
+      function done() {
+        if (dirty)
+          me._accountWidget.refresh();
+        me.onEndEdit();
+      }
+
+      commitAttributes();
     },
 
     onDeleteConf: function() {

app/controllers/accounts_controller.rb
@@ -21,42 +21,6 @@ class AccountsController < ApplicationController
     end
   end
 
-  # PUT /accounts/1
-  def update
-    if account.nil?
-      return render :nothing => true, :status => :not_found
-    end
-
-    account.name = params[:name] unless params[:name].blank?
-    if params[:currency]
-      account.currency = Currency.known?(params[:currency]) ? params[:currency] : 'USD'
-    end
-
-    # Update status
-    case params[:status]
-    when "active"
-      account.status = Constants::Status::ACTIVE
-    when "archived"
-      account.status = Constants::Status::ARCHIVED
-    end
-
-    # update balance and account type
-    if params[:enable_balance] && account.manual_account?
-      account.account_type_id = (params[:enable_balance] =~ /^(true|1|t)$/) ?
-        AccountType::MANUAL : AccountType::CASH
-    end
-
-    if account.has_balance? && params[:current_balance]
-      account.balance = params[:current_balance]
-    end
-
-    account.save!
-
-    respond_to do |format|
-      format.json { render :json => present(account) }
-    end
-  end
-
   # DELETE /accounts/1
   def destroy
     if account.nil?

app/controllers/brcm_controller.rb
@@ -7,7 +7,7 @@ class BrcmController < ApplicationController
   before_filter :check_authentication
 
   def passthrough
-    res = brcm.get(request.fullpath.sub(%r{^/data}, "")) do |req|
+    res = brcm.request(request.method, request.fullpath.sub(%r{^/data}, ""), params) do |req|
       req.user = current_user
       req.headers["Accept-Language"] = request.headers["HTTP_ACCEPT_LANGUAGE"]
       req.headers["Accept"] = request.headers["HTTP_ACCEPT"]
@@ -19,7 +19,7 @@ class BrcmController < ApplicationController
   end
 
   def transactions
-    res = brcm.get(request.fullpath.sub(%r{^/data}, "")) do |req|
+    res = brcm.request(request.method, request.fullpath.sub(%r{^/data}, ""), params) do |req|
       req.user = current_user
       req.headers["Accept-Language"] = request.headers["HTTP_ACCEPT_LANGUAGE"]
       req.headers["Accept"] = (params[:format] == 'xml') ? 'application/xml' : 'application/json'

lib/service.rb
@@ -18,14 +18,37 @@ class Service
     @auth_scheme = auth_scheme
   end
 
-  def get(path)
+  def get(path, &block)
+    request :get, path, &block
+  end
+
+  def post(path, params, &block)
+    request :post, path, params, &block
+  end
+
+  def put(path, params, &block)
+    request :put, path, params, &block
+  end
+
+  def delete(path, &block)
+    request :delete, path, &block
+  end
+
+  def request(method, path, params={}, &block)
+    request_for(path, params, &block).__send__(method.to_s.downcase)
+  end
+
+  private
+
+  def request_for(path, params={})
     # strip leading slashes for URI#+
     path = path[1..-1] if path.starts_with?('/')
 
-    return(Request.new(@base_uri + path) do |req|
+    Request.new(@base_uri + path) do |req|
       req.auth_scheme = auth_scheme if auth_scheme
+      req.params = params
       yield req if block_given?
-    end.get)
+    end
   end
 
   def self.get(name)
@@ -48,7 +71,7 @@ class Service
     attr_reader   :uri, :headers
     attr_accessor :timeout, :retries,
                   :user, :auth_scheme,
-                  :proxy_url
+                  :proxy_url, :params
 
     def initialize(uri)
       # set defaults
@@ -56,6 +79,7 @@ class Service
       @proxy_url   = nil # disable proxy
       @retries     = 2
       @auth_scheme = 'Basic'
+      @params      = {}
 
       yield self if block_given?
 
@@ -63,13 +87,31 @@ class Service
     end
 
     def get
+      perform :get
+    end
+
+    def put
+      perform :put
+    end
+
+    def post
+      perform :post
+    end
+
+    def delete
+      perform :delete
+    end
+
+    private
+
+    def perform(method)
       retries   = @retries
       old_proxy = RestClient.proxy
       res       = nil
 
       begin
         RestClient.proxy = proxy_url.blank?? nil : proxy_url
-        res = get_without_error_handling
+        res = [:put, :post].include?(method) ? resource.__send__(method, params) : resource.__send__(method)
       rescue RestClient::Exception => e
         if retries > 0
           Service.logger.warn { ["#{e.class}: #{e.message}", *e.backtrace].join("\n") }
@@ -87,13 +129,11 @@ class Service
       return Response.new(res.code, res.headers, res.body, res.headers[:content_type]) if res
     end
 
-    private
-
-    def get_without_error_handling
+    def resource
       RestClient::Resource.new(uri.to_s,
         :timeout => timeout.to_i,
         :headers => headers_for_rest_client
-      ).get
+      )
     end
 
     def headers_for_rest_client

spec/controllers/accounts_controller_spec.rb
@@ -174,62 +174,4 @@ describe AccountsController do
       end
     end
   end
-
-  describe "PUT /accounts/:id" do
-    it_should_behave_like "it has a logged-in user"
-
-    before do
-      @account = Account.make(:cash, :user => current_user)
-    end
-
-    it "should update the account name" do
-      put :update, :name => "My Checking Account", :id => @account.to_param
-      @account.reload.name.should == "My Checking Account"
-    end
-
-    it "should update the currency if the currency is known" do
-      put :update, :currency => "EUR", :id => @account.to_param
-      @account.reload.currency.should == "EUR"
-    end
-
-    it "should set the currency to USD if the currency is unknown" do
-      put :update, :currency => "FOO", :id => @account.to_param
-      @account.reload.currency.should == "USD"
-    end
-
-    it "should set the status to archived" do
-      put :update, :status => "archived", :id => @account.to_param
-      @account.reload.status.should == Constants::Status::ARCHIVED
-    end
-
-    it "should set the status back to active" do
-      @account.update_attribute(:status, Constants::Status::ARCHIVED)
-      put :update, :status => "active", :id => @account.to_param
-      @account.reload.status.should == Constants::Status::ACTIVE
-    end
-
-    it "should convert a cash account to a manual account" do
-      put :update, :enable_balance => "true", :id => @account.to_param
-      @account.reload.account_type_id.should == AccountType::MANUAL
-    end
-
-    it "should convert a manual account to a cash account" do
-      @account.update_attribute(:account_type_id, AccountType::MANUAL)
-      put :update, :enable_balance => "false", :id => @account.to_param
-      @account.reload.account_type_id.should == AccountType::CASH
-    end
-
-    it "should not update the balance if the account has not balance" do
-      @account.has_balance?.should be_false
-      put :update, :current_balance => "100", :id => @account.to_param
-      @account.reload.balance.should be_nil
-    end
-
-    it "should update the balance if the account has a balance" do
-      @account.update_attribute(:account_type_id, AccountType::MANUAL)
-      @account.should have_balance
-      put :update, :current_balance => "123.45", :id => @account.to_param
-      @account.reload.balance.to_d.should == 123.45.to_d
-    end
-  end
 end

commit 4bc7d3503ed134a093f46f11d783d37a2ee8e6a5

Fix that balances could not be edited to be negative. Closes GH-34.
Regression since 91573cc32.

public/javascripts/wesabe/views/widgets/accounts/AccountEditDialog.js
@@ -221,7 +221,7 @@ wesabe.$class('wesabe.views.widgets.accounts.AccountEditDialog', wesabe.views.wi
           $.ajax({
             type: "POST",
             url: accountURI+"/balances",
-            data: {balance: newBalance && newBalance.replace(/[^\d\.,]+/g, '')},
+            data: {balance: newBalance && newBalance.replace(/[^-\d\.,]+/g, '')},
             beforeSend: busy,
             success: done,
             error: error,